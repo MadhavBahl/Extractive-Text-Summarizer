9b4b58ca1b8441e13b62f301ae94fad942a18560
MadhavBahlMD <madhavbahl10@gmail.com>
2019-12-07 20:08:21 +0530

    Fix summarize_url

--- Summarizer_Service/summarize_url.py
+++ Summarizer_Service/summarize_url.py
@@ -28,10 +28,10 @@ def summarize_webpage (url, num_sent):
     def preprocess (text):
         # Convert to lower case
         clean_text = text.lower()
-        # Remove special characters
-        clean_text = re.sub (r"\W", " ", clean_text)
         # Remove digits
         clean_text = re.sub (r"\d", " ", clean_text)
+        # Remove special characters
+        clean_text = re.sub (r"\W", " ", clean_text)
         # Remove all the extra spaces with a single space
         clean_text = re.sub (r"\s+", " ", clean_text)
         # return the clean text
@@ -39,11 +39,7 @@ def summarize_webpage (url, num_sent):
 
     clean_text = preprocess (text)
 
-    # Tokenize the data into sentences
-    def tokenize (text):
-        return nltk.sent_tokenize (text)
-
-    sentences = tokenize (text)
+    sentences = nltk.sent_tokenize (text)
 
     # Remove stop words and create a dictionary of word-count
     stop_words = nltk.corpus.stopwords.words('english')
@@ -57,30 +53,30 @@ def summarize_webpage (url, num_sent):
             else:
                 word_count_dict[word] += 1
 
+    # Find the total number of terms (not necessarily unique) = sum of values in the word_count_dict
+    total_terms = sum(word_count_dict[term] for term in word_count_dict)
+
     # Normalize the word-frequency dictionary (weighted word count matrix/dictionary)
-    max_value = max(word_count_dict.values())
     for key in word_count_dict.keys():
-        word_count_dict[key] = word_count_dict[key]/max_value
+        word_count_dict[key] = word_count_dict[key]/total_terms
         
     # Create sentece scores
     sentence_score_dict = {}
     for sentence in sentences:
         for word in nltk.word_tokenize(sentence.lower()):
             if word in word_count_dict.keys():
-                if len(sentence.split(' ')) < 25: # 25 taken at random, to remove very long sentences
+                # Now here's a problem, very long sentences will always have high scores, so here we can ignore very long sentences
+                if len(sentence.split(' ')) < 20: # ignore sentences having words more than 20
                     if sentence not in sentence_score_dict.keys():
                         sentence_score_dict[sentence] = word_count_dict[word]
                     else:
                         sentence_score_dict[sentence] += word_count_dict[word]
 
     # Get the summary
-    best_sentences = heapq.nlargest(num_sent, sentence_score_dict, key=sentence_score_dict.get)
+    summary = heapq.nlargest(num_sent, sentence_score_dict, key=sentence_score_dict.get)
 
     # Print the summary
-    print ('/* ================================================ */')
-    for sentence in best_sentences:
-        print(sentence)
-    return best_sentences
+    return summary
 
 webpage = "https://en.wikipedia.org/wiki/Natural_language_processing"
 summarize_webpage (webpage, 10)
